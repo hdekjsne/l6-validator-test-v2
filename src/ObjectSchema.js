2644c83af0d346108ffb6616210e1db8e6a287d0
Анастасия Мамаева <hdekjsne@gmail.com>
2023-12-28 05:40:08 +0300

    final step

--- src/ObjectSchema.js
+++ src/ObjectSchema.js
@@ -1,1 +1,25 @@
-export default class {}
+export default class {
+	constructor() {
+		this.customValidator = {};
+	}
+
+	shape(validatorObj) {
+		this.customValidator = validatorObj;
+		return this;
+	}
+
+	isValid(value) {
+
+		function validateDeep(obj, validator) {
+			const validations = [];
+			for (const key of Object.keys(obj)) {
+				if (typeof obj[key] === 'object' && !Array.isArray(obj[key])) {
+					validations.push(validateDeep(obj[key], validator[key]));
+				} else validations.push(validator[key].isValid(obj[key]));
+			}
+			return !validations.includes(false);
+		}
+
+		return validateDeep(value, this.customValidator);
+	}
+}
